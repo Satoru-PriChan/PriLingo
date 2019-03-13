dc76390be77a5c9b911eabe710b453e5cc77d888
Satoru Prichan <chihayayome@yahoo.co.jp>
2019-03-13 22:37:03 +0900

    [PH1.0][UI] added MainPageViewController and it's xib.

---
+++ PriLingo/UI/MainPageViewController.swift
@@ -0,0 +1,33 @@
+//
+//  MainPageViewController.swift
+//  PriLingo
+//
+//  Created by USER on 2019/03/13.
+//  Copyright © 2019 SoLaMi Smile. All rights reserved.
+//
+
+import UIKit
+
+class MainPageViewController: UIViewController {
+
+    @IBOutlet weak var myImageView: UIImageView!
+    @IBOutlet weak var myCollectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
