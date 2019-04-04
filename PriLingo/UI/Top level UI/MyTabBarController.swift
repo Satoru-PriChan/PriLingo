66d29ff36ee672bc2d92ac0d734c19de75dbdac3
Satoru Prichan <chihayayome@yahoo.co.jp>
2019-04-04 19:03:09 +0900

    [PH1.0][UI] UITabBar completed. The previous one is a mistake.

--- PriLingo/UI/Top level UI/MyTabBarController.swift
+++ PriLingo/UI/Top level UI/MyTabBarController.swift
@@ -53,8 +53,7 @@ class MyTabBarController: UITabBarController {
     func addTabBarShape() {
         let _shapeLayer = CAShapeLayer()
       _shapeLayer.path = self.createPathCircle()
-        _shapeLayer.strokeColor = UIColor.init(white: 0.5, alpha: 0.9).cgColor
-        _shapeLayer.fillColor = UIColor.init(patternImage: UIImage.init(named: "test10.png")!).cgColor
+        _shapeLayer.fillColor = UIColor.init(patternImage: UIImage.init(named: "test12.png")!).cgColor
         
         //this way, it can support orientation change.
         if  (self.tabBar.layer.sublayers != nil) && self.tabBar.layer.sublayers!.count != 0 {
@@ -83,11 +82,8 @@ class MyTabBarController: UITabBarController {
         
         //starting point (left top) then draw lines until the it retunrs to the starting point by close().
         let startX: CGFloat = 0
-        let startY: CGFloat = -100
+        let startY: CGFloat = -37
         path.move(to: CGPoint.init(x: startX, y: startY))
-        path.addLine(to: CGPoint.init(x: (centerWidth - radius), y: startY))
-        //radian = degree * π/180
-        path.addArc(withCenter: CGPoint.init(x: centerWidth, y: startY), radius: radius, startAngle: CGFloat(180) * CGFloat.pi / 180, endAngle: CGFloat(0) * CGFloat.pi / 180, clockwise: true)
         path.addLine(to: CGPoint.init(x: self.tabBar.frame.width, y: startY))
         path.addLine(to: CGPoint.init(x: self.tabBar.frame.width, y: self.tabBar.frame.height))
         path.addLine(to: CGPoint.init(x: startX, y: self.tabBar.frame.height))
