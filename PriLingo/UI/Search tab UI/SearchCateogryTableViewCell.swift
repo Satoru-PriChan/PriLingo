cb4a2ff1cee636635fc1478486ab6de4e447d63b
Satoru Prichan <chihayayome@yahoo.co.jp>
2019-05-06 12:08:03 +0900

    [PH1.0][UI] Modified the design of CategorySearchTableViewCell.

---
+++ PriLingo/UI/Search tab UI/SearchCateogryTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  SearchCateogryTableViewCell.swift
+//  PriLingo
+//
+//  Created by USER on 2019/05/06.
+//  Copyright © 2019 SoLaMi Smile. All rights reserved.
+//
+
+import UIKit
+
+class SearchCateogryTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var chapterLabel: UILabel!
+    @IBOutlet weak var categoryLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
