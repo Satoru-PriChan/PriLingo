0bdd5fe8322b90bb5699308e9dd1b5cd5562bf31
Satoru Prichan <chihayayome@yahoo.co.jp>
2019-05-15 12:55:14 +0900

    [PH1.0][UI] Fixed a bug that the back indicator image of UINavigationBar was totally blue.

--- PriLingo/UI/Top level UI/MyTabBarController.swift
+++ PriLingo/UI/Top level UI/MyTabBarController.swift
@@ -32,8 +32,8 @@ class MyTabBarController: UITabBarController {
         viewControllers.forEach {
 
             $0.navigationBar.barTintColor = UIColor.init(patternImage: UIImage.init(named: "PinkPolkaDot2.png")!)
-            $0.navigationBar.backIndicatorImage = UIImage.init(named: "BackArrow.png")
-            $0.navigationBar.backIndicatorTransitionMaskImage = UIImage.init(named: "BackArrow.png")
+            $0.navigationBar.backIndicatorImage = UIImage.init(named: "BackArrow.png")?.withRenderingMode(.alwaysOriginal)
+            $0.navigationBar.backIndicatorTransitionMaskImage = UIImage.init(named: "BackArrow.png")?.withRenderingMode(.alwaysOriginal)
         }
         self.viewControllers = viewControllers
         
