16f059fb592431fd9ca87342fb5d111c7d4ccc15
Satoru Prichan <chihayayome@yahoo.co.jp>
2019-05-17 11:11:32 +0900

    [PH1.0][UI] Added custom navigation controller and custom navigation bar.

---
+++ PriLingo/UI/Top level UI/MyNavigationBar.swift
@@ -0,0 +1,21 @@
+//
+//  MyNavigationBar.swift
+//  PriLingo
+//
+//  Created by USER on 2019/05/17.
+//  Copyright © 2019 SoLaMi Smile. All rights reserved.
+//
+
+import UIKit
+
+class MyNavigationBar: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ PriLingo/UI/Top level UI/MyNavigationController.swift
@@ -0,0 +1,14 @@
+//
+//  MyNavigationController.swift
+//  PriLingo
+//
+//  Created by USER on 2019/05/17.
+//  Copyright © 2019 SoLaMi Smile. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MyNavigationController: UINavigationController {
+    
+}
