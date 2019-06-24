c8e3f1be10310bc4f1bf255607a4d99077b15611
Satoru Prichan <chihayayome@yahoo.co.jp>
2019-06-25 06:56:41 +0900

    [PH1.0][UI] Added LanguagedisplayOrderVC and PreferredLanguageVC.

---
+++ PriLingo/UI/Settings tab UI/LanguageDisplayOrderViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LanguageDisplayOrderViewController.swift
+//  PriLingo
+//
+//  Created by USER on 2019/06/25.
+//  Copyright © 2019 SoLaMi Smile. All rights reserved.
+//
+
+import UIKit
+
+class LanguageDisplayOrderViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
